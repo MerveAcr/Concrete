ef7bafd55c91213be04d4afed4252a4a98912baf
merveacar <merveacar9192@gmail.com>
2021-12-31 00:21:46 +0300

    2Commit

--- src/deneme2.java
+++ src/deneme2.java
@@ -7,9 +7,14 @@ public class deneme2 {
        return  "Hello "+ name;
     }
 
+    public static int sum2(int b, int c){
+        return b+c;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(20,5));
         System.out.println(text("merve"));
+        System.out.println(sum2(20,5));
     }
 
 }
