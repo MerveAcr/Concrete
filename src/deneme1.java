204e94bee16237f6a83fadba50e8c1e11800a90a
merveacar <merveacar9192@gmail.com>
2022-01-01 19:43:20 +0300

    add variable

--- src/deneme1.java
+++ src/deneme1.java
@@ -2,5 +2,8 @@ public class deneme1 {
     public static void main(String[] args) {
         System.out.println("Hi merve !");
         System.out.println("Hi, how are you?");
+
+        String isim="merve";
+        int yas=30;
     }
 }
